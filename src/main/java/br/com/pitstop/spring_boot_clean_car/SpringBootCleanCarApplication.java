57cad5592000486530637a400f9df00f956e4209
Cesar Augusto <cesarprofissional22@gmail.com>
2026-07-11 20:13:23 -0300

    spring initializr

---
+++ src/main/java/br/com/pitstop/spring_boot_clean_car/SpringBootCleanCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.pitstop.spring_boot_clean_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCleanCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCleanCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pitstop/spring_boot_clean_car/SpringBootCleanCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pitstop.spring_boot_clean_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCleanCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
